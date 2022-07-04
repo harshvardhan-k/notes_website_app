9b4ced22c2b84713f6d88d695a05439dbfe7fd17
Harshvardhan Kushwaha <79045275+harshvardhan-k@users.noreply.github.com>
2022-07-04 17:33:32 +0530

    Update notesapp.js

--- notesapp.js
+++ notesapp.js
@@ -1,83 +1,83 @@
-let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
-  let notes = localStorage.getItem("notes");
-  var d = Date();
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let myobj = {
-    text: notes,
-    text: addTxt.value,
-    date: d
-  }
-  notesObj.push(myobj);
+const addBtn=document.getElementById('addBtn');
+const titleTab=document.querySelector("input");
+const descTab=document.querySelector("textarea");
+const appear=document.getElementById('Appear');
+const notes=JSON.parse(localStorage.getItem("note")||"[]");
+const NoteHead=document.getElementById("addNote1");
+const NoteCont=document.getElementById("addNote2");
+const searching=document.getElementById("searchNote");
 
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
+function showNotes(){
+    document.querySelectorAll(".card").forEach(note => note.remove());
+    notes.forEach((note,index)=>{
+        let newCreature=`
+        <div class="card border-primary mb-3" style="max-width: 18rem; min-height:13rem">
+        <div class="card-header"><h5 class="card-title">${note.title}</h5></div>
+        <div class="card-body text-primary">
+          <p class="card-text">${note.desc}</p>
+        </div>
+        <ul class="menu">
+            <span onclick="editNote(${index},'${note.title}','${note.desc}')" class="iconify" data-icon="ant-design:edit-outlined" id="ab"></span>
+            <span onclick="deleteNote(${index})" class="iconify" data-icon="ant-design:delete-outlined" id="cd"></span>
+        </ul>
+        <hr>
+        <div id="belowhr">
+        <p id="dt">${note.time}</p>
+        </div>
+      </div>`;
+      appear.insertAdjacentHTML("afterend",newCreature);
+    })
+}
+showNotes();
+addBtn.addEventListener('click',()=>{
+    let noteTitle=titleTab.value; 
+    let noteDesc=descTab.value;
+    if(noteTitle || noteDesc){
+        let timenow= Date();
+        let noteInfo={
+            title:noteTitle,
+            desc:noteDesc,
+            time:timenow
+        }
+        notes.push(noteInfo);
+        localStorage.setItem("note",JSON.stringify(notes));
+    }
+    showNotes();
+    titleTab.value="";
+    descTab.value="";
+})
 
-  showNotes();
-});
 
-function showNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let html = "";
-  notesObj.forEach(function(element, index) {
-    html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <b><p class="card-text"> ${element.text}</p></b>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button><br>
-                        <i>${element.date}</i>
-                    </div>
-                </div>`;
-  });
-  let notesElm = document.getElementById("notes");
-  if (notesObj.length != 0) {
-    notesElm.innerHTML = html;
-  } else {
-    notesElm.innerHTML = 'Your Notes Will Appear here';
-  }
+function deleteNote(noteIndex){
+    notes.splice(noteIndex,1);
+    localStorage.setItem("note",JSON.stringify(notes));
+    showNotes();
 }
 
-function deleteNote(index) {
-
-
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-
-  notesObj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
+function editNote(ID,TITLE,DESC){
+    if(NoteHead.value=="" && NoteCont.value==""){
+    NoteHead.value=TITLE;
+    NoteCont.value=DESC;
+    deleteNote(ID);
+    }
+    else{
+        alert("Please clear the input textareas first");
+    }
 }
 
 
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
- 
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
+searching.addEventListener("input",function(){
+    let inpUT=searching.value.toLowerCase();
+    console.log('input'.inp);
+    let available=document.getElementsByClassName("card border-primary mb-3");
+    Array.from(available).forEach(function(e){
+        let txt1=e.getElementsByTagName("h5")[0].innerText;
+        let txt2=e.getElementsByTagName("p")[0].innerText;
+        if(txt1.includes(inpUT) || txt2.includes(inpUT)){
+            e.style.display="block";
         }
         else{
-            element.style.display = "none";
+            e.style.display="none";
         }
-        
     })
 })
